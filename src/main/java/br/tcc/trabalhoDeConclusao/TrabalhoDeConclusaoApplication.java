f48fb7c96f66c0c2a08b18879e00c8aa33b5d2e2
kauan-souza <kauanszcorreia@gmail.com>
2022-05-18 20:41:32 -0300

    teste

---
+++ src/main/java/br/tcc/trabalhoDeConclusao/TrabalhoDeConclusaoApplication.java
@@ -0,0 +1,13 @@
+package br.tcc.trabalhoDeConclusao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoDeConclusaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoDeConclusaoApplication.class, args);
+	}
+
+}
